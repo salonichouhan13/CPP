05e09c4f1576ae3df15c619670935ed8d16ddd52
Saloni Chouhan <salonichouhan803@gmail.com>
2026-06-30 22:45:46 +0530

    prime f

--- Maths_Basic/prime_No.cpp
+++ Maths_Basic/prime_No.cpp
@@ -1,21 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-  int n;
-  int count = 0;
-  cout<<"enter num to check"<<endl;
-  cin>>n;
+bool Prime(int n)//{
+//   int n;
+//   int count = 0;
+//   cout<<"enter num to check"<<endl;
+//   cin>>n;
 
-  for(int i=1; i<=n; i++){
-    if(n%i==0){
-      count= count+1;
-    }
-  }
-  if(count==2){
-      cout<<"number is prime";}
-      else{
-        cout<<"number is not prime"<<endl;
-      }
+//   for(int i=1; i<=n; i++){
+//     if(n%i==0){
+//       count= count+1;
+//     }
+//   }
+//   if(count==2){
+//       cout<<"number is prime";}
+//       else{
+//         cout<<"number is not prime"<<endl;
+//       }
   
-  return 0;
+//   return 0;
+// }
+{ if(n<=1)
+    return false;
+    for(int i =2; i*i<=n; i++){
+      if(n%1==0)
+       return false;
+}return true;
 }
---
+++ Maths_Basic/prime_factor.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void primef(int n){
+  for(int i =2; i*i<=n; i++){
+    while(n%i==0){
+      cout<<i<<" ";
+      n = n/i;
+    }
+  }
+  if(n!=1){
+    cout<<n;
+  }
+}
+  int main(){
+     primef(780);
+  }
